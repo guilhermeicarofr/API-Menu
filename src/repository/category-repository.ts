57add660313f37c029647170a493b8e52a22cdad
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-04-10 03:44:39 -0300

    fix: fixed validation of Post new product categories array, added population of reference categories and parent field on repositories

--- src/repository/category-repository.ts
+++ src/repository/category-repository.ts
@@ -1,23 +1,34 @@
+import { isValidObjectId } from 'mongoose';
+
+import { Cache } from 'database/cache';
 import { Database } from 'database/database';
 import { ICategory } from 'model/ICategory';
-import { isValidObjectId } from 'mongoose';
 
 export class CategoryRepository {
   private db;
+  private cache;
 
   constructor() {
     this.db = Database.getInstance();
+    this.cache = Cache.getInstance();
     this.db.connect();
+    this.cache.connect();
   }
 
   async findAll(): Promise<ICategory[]> {
-    const categories = await this.db.Category.find();
+    let categories = await this.cache.getCategories();
+    if(!categories) {
+      categories = await this.db.Category.find().populate('parent');
+      await this.cache.saveCategories(categories);
+    }
     return categories;
   }
 
   async create(category: ICategory) {
+    await this.cache.flushCategories();
+    
     const newCategory = new this.db.Category(category);
-    return await newCategory.save();
+    return (await newCategory.save());
   }
 
   async findById(id: string) {
--- src/repository/product-repository.ts
+++ src/repository/product-repository.ts
@@ -1,38 +1,51 @@
+import { isValidObjectId } from 'mongoose';
+
+import { Cache } from 'database/cache';
 import { Database } from 'database/database';
 import { IProduct } from 'model/IProduct';
-import { isValidObjectId } from 'mongoose';
 
 export class ProductRepository {
   private db;
+  private cache;
 
   constructor() {
     this.db = Database.getInstance();
+    this.cache = Cache.getInstance();
     this.db.connect();
+    this.cache.connect();
   }
 
   async findAll(): Promise<IProduct[]> {
-    const products = await this.db.Product.find();
+    let products = await this.cache.getProducts();
+    if(!products) {
+      products = await this.db.Product.find().populate({ path: 'categories', populate: { path: 'parent' } });
+      await this.cache.saveProducts(products);
+    }
     return products;
   }
 
   async create(product: IProduct) {
+    await this.cache.flushProducts();
+
     const newProduct = new this.db.Product(product);
-    return await newProduct.save();
+    return (await newProduct.save()).populate({ path: 'categories', populate: { path: 'parent' } });
   }
 
   async findById(id: string) {
     let product = null;
     if(isValidObjectId(id)) {
-      product = await this.db.Product.findById({ _id: id });
+      product = await this.db.Product.findById({ _id: id }).populate({ path: 'categories', populate: { path: 'parent' } });
     }
     return product;
   }
 
   async delete(id: string) {
+    await this.cache.flushProducts();
     await this.db.Product.findByIdAndDelete({ _id: id });
   }
 
   async patch(id: string, newData: IProduct) {
+    await this.cache.flushProducts();
     await this.db.Product.findByIdAndUpdate({ _id: id }, newData);
   }
 }
